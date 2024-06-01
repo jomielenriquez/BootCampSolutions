ccbb61e8e53a8d38d66be4f282966618693d8ddf
Jomiel Enriquez <77730490+jomielenriquez@users.noreply.github.com>
2024-06-01 00:34:34 +0000

    show name

--- Problem1/obj/Debug/net8.0/Problem1.AssemblyInfo.cs
+++ Problem1/obj/Debug/net8.0/Problem1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Problem1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48b7f276512695678cd73408b532717a556a5402")]
 [assembly: System.Reflection.AssemblyProductAttribute("Problem1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Problem1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
